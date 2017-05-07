90709f9f2acbab67de5fc1812681a52519dcdfa6
OmarReal <real25130@gmail.com>
2017-05-07 19:13:39 +0300

    OMAR-REAL

---
+++ login.php
@@ -0,0 +1,100 @@
+<html lang="en">
+<div class="singup">
+<center>
+<br /><br /><br /><br /><br /><br />
+<div class="title">
+<b>
+Login
+</b>
+</div>
+<div class="for">
+<div class="inpu">
+<br /><br />
+<form action="slog.php" method="get">
+    
+<b>
+Username 
+</b>
+<br />
+<input type="text" name="log_username"/>
+</li>
+<br />
+<b>
+Password
+</b>
+<br />
+<input type="password" name = "log_pass"/>
+</li>
+<br />
+</div>
+<div class="log">
+<center>
+<br />
+&nbsp<input type="submit" name="login" value="Login" style="background-color:#2C99F0;border:0px;height:30px;color:#fff;width:50px;border-radius:18px;"/>
+<br />
+<span>
+<a href='https://ewq-phpcource.c9users.io/singup.php'> Creat New Acount </a>
+</span>
+</form>
+</center>
+</div>
+</div>
+</center>
+</div>
+
+<style>
+*{
+padding:0px;
+margin:0px;
+}
+
+.singup{
+background:#000;
+width:100%;
+height:100%;
+}
+
+.for{
+color:#fff;
+text-align:center;
+width:400px;
+height:250px;
+background-color:rgba(255, 0, 0, 0.4);
+border-bottom:1px;
+border-radius:0px 0px 95px 95px;
+margin-bottom:4px;
+}
+
+form input{
+border:0px;
+border-radius:22px;
+height:20px;
+color:#000;
+}
+
+form input:hover{
+background-color:#000;
+color:#fff;
+}
+
+.title{
+color:#000;
+height:50px;
+width:400px;
+background:#fff;
+border-radius:0px 0px 0px 0px;
+font-size:30px;
+}
+
+a{
+text-decoration:none;
+color:#fff;
+line-height:3;
+}
+
+span{
+color:#fff;
+font-size:20px;
+}
+
+</style>
