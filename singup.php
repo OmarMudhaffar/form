114ddf071ac55950cc6f570f545cc226e430b550
OmarReal <real25130@gmail.com>
2017-05-07 19:11:36 +0300

    OMAR-REAL

---
+++ singup.php
@@ -0,0 +1,111 @@
+<head>
+<title>
+Sing Up
+</title>
+</head>
+
+<div class='singup'>
+<center>
+<br /><br /><br /><br /><br /><br />
+<div class='title'>
+<b>
+Sing Up 
+</b>
+</div>
+<div class='for'>
+<div class='inpu'>
+<br /><br />
+<form action='save.php' method='get'>
+    
+<b>
+Username 
+</b>
+<br />
+<input type='text' name='username'/> 
+</li>
+<br />
+<b>
+Password
+</b>
+<br />
+<input type='password' name = 'pass'>
+</li>
+<br />
+<b>
+Email
+</b>
+<br />
+<input type='email' name='email'>
+<br />
+</div>
+<div class='log'>
+<center>
+<br />
+<input type='submit' name='singup' value='Sing Up' style='background-color:#2C99F0;border:0px;height:30px;color:#fff;width:50px;border-radius:12px;'/>
+</form>
+<br />
+<span>
+<a href='https://ewq-phpcource.c9users.io/login.php'> Do You Have Acount ? </a>
+</span>
+</center>
+</div>
+</div>
+</center>
+</div>
+
+<style>
+*{
+padding:0px;
+margin:0px;
+}
+
+.singup{
+background:#000;
+width:100%;
+height:100%;
+}
+
+.for{
+color:#fff;
+text-align:center;
+width:400px;
+height:300px;
+background-color:rgba(255, 0, 0, 0.4);
+border-bottom:1px;
+border-radius:0px 0px 95px 95px;
+margin-bottom:4px;
+}
+
+form input{
+border:0px;
+border-radius:22px;
+height:20px;
+color:#000;
+}
+
+form input:hover{
+background-color:#000;
+color:#fff;
+}
+
+.title{
+color:#000;
+height:50px;
+width:400px;
+background:#fff;
+border-radius:0px 0px 0px 0px;
+font-size:30px;
+}
+
+a{
+text-decoration:none;
+color:#fff;
+line-height:3;
+}
+
+span{
+color:#fff;
+font-size:20px;
+}
+
+</style>
